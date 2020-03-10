312221863599b0bc4bdfa32d72f0a00352f8176e
Lukas Snoek <lukassnoek@gmail.com>
2020-03-10 21:10:25 +0100

    Fs stuff; now proper derivatives

--- code/run_fmriprep.sh
+++ code/run_fmriprep.sh
@@ -1,8 +1,9 @@
 bids_dir=..
+work_dir=../../fmriprep_work
 out_dir=$bids_dir/derivatives
 n_jobs=4
 
-subs=(`ls -d1 $bids_dir/sub-*`)
+subs=(`ls -d1 $bids_dir/sub-0004`)
 # Run subjects one at the time as to avoid memory issues
 
 i=0
@@ -19,16 +20,15 @@ for sub in ${subs[@]}; do
     fmriprep-docker $bids_dir $out_dir \
         --image poldracklab/fmriprep:1.4.1 \
         --participant-label $label \
-        --nthreads 2 \
+        --nthreads 3 \
         --omp-nthreads 2 \
         --ignore slicetiming \
         --output-spaces T1w MNI152NLin2009cAsym fsaverage5 \
         --use-syn-sdc \
 	--skip-bids-validation \
 	-u 1002:1002 \
-	-w $out_dir/fmriprep_work \
+	-w $work_dir \
         --fs-license-file /usr/local/freesurfer/license.txt \
-	--patch-fmriprep $PWD/fmriprep/fmriprep \
         --notrack &
 
     i=$(($i + 1))
